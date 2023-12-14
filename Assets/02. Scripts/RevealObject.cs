1c76c825fe60a38a2160952807f304779c62ba6d
mynzi1 <mynzi00@sookmyung.ac.kr>
2023-12-14 23:16:21 +0900

    feat. Add flashlight

---
+++ Assets/02. Scripts/RevealObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class RevealObject : MonoBehaviour
+{
+    [SerializeField] Material Mat;
+    [SerializeField] Light SpotLight;
+
+    void Start()
+    {
+        Mat = GetComponent<Renderer>().sharedMaterial;
+    }
+	
+	void Update ()
+    {
+        Mat.SetVector("MyLightPosition",  SpotLight.transform.position);
+        Mat.SetVector("MyLightDirection", -SpotLight.transform.forward );
+        //Mat.SetFloat ("MyLightAngle", SpotLight.spotAngle         );
+    }
+}
